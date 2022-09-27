77a0a10c6863bd3eab0425335944400d6ab2dc45
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-09-27 12:07:04 +0300

    commit tree 2c3ebe63552ab6aed84dd170df3db2e6ec966c1c

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+        
+    end
+    def grades
+        grades = Student.order(grade: :desc)
+        render json: grades
+    end
+    def highest_grade
+        highest = Student.order(grades::desc).first
+        render json: highest  
+    end
+end
